8aba8fa9f684f1b01328420205154996ee99d258
jasmine-22 <116296818+jasmine-22@users.noreply.github.com>
2022-11-02 12:22:33 -0400

    added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-orange": "#EE64431A",
+        green: "#1AA800",
+        gray: "#9B9B9B",
+      },
+    },
+  },
+  plugins: [],
+};
